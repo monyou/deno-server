c0c349a6f11e2f1c76654288b1233a7d6738cb14
Simeon Mechkov <montishak@gmail.com>
2025-07-15 22:33:01 +0300

    opa

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export type { Context, Next } from "jsr:@hono/hono";
 export { serveStatic, cors } from "https://deno.land/x/hono/middleware.ts";
 export { setCookie, getCookie, deleteCookie } from "jsr:@hono/hono/cookie";
 export { compile } from "npm:pug@3.0.3";
-export { MongoClient } from "npm:mongodb@6.17.0";
+export { MongoClient, ObjectId } from "npm:mongodb@6.17.0";
 export { hash, verify } from "jsr:@denorg/scrypt@4.4.4";
--- middlewares/movie_match/cookieAuth.ts
+++ middlewares/movie_match/cookieAuth.ts
@@ -1,4 +1,4 @@
-import { Context, deleteCookie, getCookie, Next } from "../../deps.ts";
+import { Context, deleteCookie, getCookie, Next, ObjectId } from "../../deps.ts";
 import { closeMongoDbConnection, openMongoDbConnection } from "../../utils/movie_match/mongoDbClient.ts";
 
 export const authCookie = "sm_movie_match_auth";
@@ -7,20 +7,24 @@ const cookieAuthMiddleware = async (ctx: Context, next: Next) => {
     const cookie = getCookie(ctx, authCookie);
     if (!cookie) {
         deleteCookie(ctx, authCookie, {
-            path: '/',
+            httpOnly: true,
             secure: ctx.get("isSecure"),
+            sameSite: ctx.get('isSecure') ? "none" : "lax",
+            path: "/"
         });
         ctx.status(401);
         return ctx.json({ message: "Session expired" });
     }
     const db = await openMongoDbConnection();
     const usersCollection = db.collection("User");
-    const user = await usersCollection.findOne({ _id: cookie, active: true });
+    const user = await usersCollection.findOne({ _id: new ObjectId(cookie), active: true });
     await closeMongoDbConnection();
     if (!user) {
         deleteCookie(ctx, authCookie, {
-            path: '/',
+            httpOnly: true,
             secure: ctx.get("isSecure"),
+            sameSite: ctx.get('isSecure') ? "none" : "lax",
+            path: "/"
         });
         ctx.status(403);
         return ctx.json({ message: "Forbidden" });
