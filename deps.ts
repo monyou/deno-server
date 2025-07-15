136f6caf445c6a82b669ecff48a588d5a5903dfb
Simeon Mechkov <montishak@gmail.com>
2025-07-15 18:43:04 +0300

    deps

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ export { Application, Router, send } from "jsr:@oak/oak";
 export type { Next, Context } from "jsr:@oak/oak";
 export { compile } from "npm:pug@3.0.3";
 export { MongoClient } from "npm:mongodb@6.17.0";
-export { hash, compare } from "jsr:@da/bcrypt";
+export { hash, verify } from "jsr:@denorg/scrypt@4.4.4";
 
 
 
