51505712c8c0ccdb77bafe9f1da71d8ed632e308
Simeon Mechkov <montishak@gmail.com>
2025-07-15 13:38:44 +0300

    New deploy

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 import "jsr:@std/dotenv/load";
+
 export { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 export type { Next, Context } from "https://deno.land/x/oak/mod.ts";
 export * as pug from "https://esm.sh/pug";
